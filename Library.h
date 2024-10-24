f0937a5266fd5a6b1ab0c31cfa802f56f4a23497
Josiah Tripp <jtripp6@ut.utm.edu>
2024-10-24 12:46:14 -0500

    Changed std::string typedef to str

--- Library.h
+++ Library.h
@@ -12,18 +12,17 @@
 #include <string>
 #include <list>
 
-typedef std::string __string;
+typedef std::string str;
 
 typedef struct book{
 
-    __string title;
-    __string author;
+    str title;
+    str author;
     int pages;
-    __string isbn;
+    str isbn;
     float cover_price;
     int year;
 } book;
-
 
 /**
  * Holds a linked-list of books. Can search, sort (when loading in), delete, and read in & write out books to a file.
@@ -42,12 +41,12 @@ class Library{
         ~Library();
         void push_front(book __book);
         void push_back(book __book);
-        bool read_from_file(__string filename);// Return false if file can't be opened
-        bool write_to_file(__string filename);// Return false if the file can't be opened
+        bool read_from_file(str filename);// Return false if file can't be opened
+        bool write_to_file(str filename);// Return false if the file can't be opened
         void insert_sorted(book __book);
-        bool find_author(__string author);// Return true if author's name is found, false if not
-        bool find_album(__string title);// Return true if book title is found, false if not
+        bool find_author(str author);// Return true if author's name is found, false if not
+        bool find_album(str title);// Return true if book title is found, false if not
         void print();
-        bool delete_book(__string title, __string author);// Return true if book was found and delete, false if not found (and not deleted)
+        bool delete_book(str title, str author);// Return true if book was found and delete, false if not found (and not deleted)
 };
 #endif
