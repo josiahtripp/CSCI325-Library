e4a6e89a3a9c67d8750d79c7db43c1b2e3cf7ba2
josiahtripp <josiahtripp@gmail.com>
2024-11-08 15:20:49 -0600

    Merged Elise's method implementations

--- Library.cpp
+++ Library.cpp
@@ -12,28 +12,8 @@
 #include <iomanip>
 #include <string>
 #include <fstream>
-#include <iomanip>
 using namespace std;
 
-<<<<<<< HEAD
-
-Library::Library() : book_count(0) {}
- 
-
-
-Library::~Library(){
-
-}
-
-void Library::push_front(book __book){
-  books.push_front(__book);
-    ++book_count;
-}
-
-void Library::push_back(book __book){
-books.push_back(__book);
-    ++book_count;
-=======
 void Library::push_front(book __book){
 
     books.push_front(__book);
@@ -42,28 +22,10 @@ void Library::push_front(book __book){
 void Library::push_back(book __book){
 
     books.push_back(__book);
->>>>>>> 178337795b5552b0e119651b72c226322cd3f453
 }
 
 bool Library::read_from_file(string filename){
-  ifstream file(filename);
-    if (!file.is_open()) {
-        cerr << "Unable to open file: " << filename << endl;
-        return false;
-    }
 
-<<<<<<< HEAD
-    books.clear();
-    book __book;
-    while (file >> ws && getline(file, __book.title)) {
-        getline(file, __book.author);
-        file >> __book.pages >> __book.isbn >> __book.cover_price >> __book.year;
-        file.ignore(); // Ignore the newline after the year field
-        insert_sorted(__book);
-    }
-    file.close();
-    cout << "Library loaded successfully from " << filename << endl;
-=======
     ifstream read_in;
     read_in.open(filename);
 
@@ -116,28 +78,11 @@ bool Library::read_from_file(string filename){
     }
     
     read_in.close();
->>>>>>> 178337795b5552b0e119651b72c226322cd3f453
     return true;
 }
 
 bool Library::write_to_file(string filename){
-ofstream file(filename);
-    if (!file.is_open()) {
-        cerr << "Could not open the file: " << filename << endl;
-        return false;
-    } for (const auto &b : books) {
-        file << b.title << '\n'
-             << b.author << '\n'
-             << b.pages << '\n'
-             << b.isbn << '\n'
-             << fixed << setprecision(2) << b.cover_price << '\n'
-             << b.year << '\n';
-    }
 
-<<<<<<< HEAD
-    file.close();
-    cout << "Library saved successfully to " << filename << endl;
-=======
     ofstream write_out;
     write_out.open(filename);
 
@@ -147,37 +92,33 @@ ofstream file(filename);
     }
 
     // Print out all
-    for(list<book>::iterator it = books.begin(); it != books.end(); it++){
+    for(list<book>::iterator it = books.begin(); it != books.end();){
 
         write_out << it->title << '\n' << it->author << '\n';
         write_out << it->year << ' ' << it->isbn << ' ';
         write_out << it->pages << ' ' << it->cover_price;
 
-        list<book>::iterator tmp_it = it;
-
-        if(++tmp_it != books.end()){// Add newline if not the last entry
+        if(++it != books.end()){// Add newline if not the last entry (and increment iterator)
             write_out << '\n';
         }
     }
     
     write_out.close();
->>>>>>> 178337795b5552b0e119651b72c226322cd3f453
     return true;
 }
 
-
-
 void Library::insert_sorted(book __book){
+
   auto it = books.begin();
     while (it != books.end() && it->author < __book.author) {
         ++it;
     }
     books.insert(it, __book);
-    ++book_count;
 }
 
 bool Library::find_author(string author){
- bool found = false;
+
+    bool found = false;
     for (const auto &b : books) {
         if (b.author == author) {
             cout << "Title: " << b.title << " (" << b.year << ")" << endl;
@@ -191,6 +132,7 @@ bool Library::find_author(string author){
 }
 
 bool Library::find_album(string title){
+
 bool found = false;
     for (const auto &b : books) {
         if (b.title == title) {
@@ -210,20 +152,7 @@ bool found = false;
 }
 
 void Library::print(){
-    if (books.empty()) {
-        cout << "Library is empty." << endl;
-        return;
-    }
 
-<<<<<<< HEAD
-    for (const auto &b : books) {
-        cout << "Title: " << b.title << ", "
-             << "Author: " << b.author << ", "
-             << "Pages: " << b.pages << ", "
-             << "ISBN: " << b.isbn << ", "
-             << "Price: $" << fixed << setprecision(2) << b.cover_price << ", "
-             << "Year: " << b.year << endl;
-=======
     for(book current : books){
 
         cout << "'" << current.title << "' by " << current.author << endl;
@@ -231,22 +160,10 @@ void Library::print(){
         cout << "\tISBN: " << current.isbn << endl;
         cout << "\tPages: " << current.pages << endl;
         cout << "\tCover Price: " << current.cover_price << "\n\n";
->>>>>>> 178337795b5552b0e119651b72c226322cd3f453
     }
 }
 
 bool Library::delete_book(string title, string author){
-<<<<<<< HEAD
- for (auto it = books.begin(); it != books.end(); ++it) {
-        if (it->title == title && it->author == author) {
-            books.erase(it);
-            --book_count;
-            cout << "Book deleted successfully.\n";
-            return true;
-        }
-    }
-    cout << "Book not found.\n";
-=======
 
     for(list<book>::iterator it = books.begin(); it != books.end(); it++){// Iterare through books
 
@@ -257,7 +174,6 @@ bool Library::delete_book(string title, string author){
             return true;
         }
     }
->>>>>>> 178337795b5552b0e119651b72c226322cd3f453
     return false;
 }
 
